5d8cdbc487c5423b8891b2c2fa51b1d2d28bb0ae
zj <zhangjie850513@126.com>
2018-01-05 11:51:58 +0800

    com

---
+++ zjTestJenkins/src/main/java/zjTestAppG/zjTestJenkins/AppTest.java
@@ -0,0 +1,22 @@
+package zjTestAppG.zjTestJenkins;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+   @Test
+   public void test01() {
+	    String method = Thread.currentThread() .getStackTrace()[1].getMethodName();
+		System.out.println("method:"+method);
+		String x = this.getClass().getName() ;
+		String y = this.getClass().getSimpleName();
+		String z = this.getClass().getCanonicalName();
+		long id = Thread.currentThread().getId();
+		System.out.println("getName:"+x);
+		System.out.println("getSimpleName:"+y);
+		System.out.println("getCanonicalName:"+z);
+		System.out.println("id:"+id);
+		System.out.println("update:");
+   }
+}
